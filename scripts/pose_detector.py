8165ee82c3b3d6d7c1096e47c8042bc9540edbf4
Kacper <kandahari1461@gmail.com>
2023-04-25 11:47:33 +0200

    Made the whole thing more readable

---
+++ scripts/pose_detector.py
@@ -0,0 +1,124 @@
+import rospy
+import cv2
+import mediapipe as mp
+import time
+from geometry_msgs.msg import Twist
+
+# ROS setup
+rospy.init_node('nerf_turret_camera_node', anonymous=True)
+command_publisher = rospy.Publisher("/nerf_turret/command",Twist,queue_size=1)
+
+
+# Mediapipe setup
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_pose = mp.solutions.pose
+
+# "webcam" input:
+cap = cv2.VideoCapture(0)
+
+# Limiting max possible FPS
+fps_limit = 200
+startTime = time.time()
+
+def draw_crosshair(image):
+  """ Takes cv2 image and returns the same one with CS 1.6 style crosshair in the middle.
+  
+  Parameters
+  ----------
+  image: cv2 compatybile image format
+  
+  Returns
+  -------
+  image: 
+  """
+  
+  (h,w,c) = image.shape
+  image_with_crosshair = cv2.line(image,(int(w/2)-20, int(h/2)),(int(w/2 - 10), int(h/2)),(0,255,0),thickness=2)
+  image_with_crosshair = cv2.line(image,(int(w/2)+20, int(h/2)),(int(w/2 + 10), int(h/2)),(0,255,0),thickness=2)
+  image_with_crosshair = cv2.line(image,(int(w/2), int(h/2+20)),(int(w/2), int(h/2 + 10)),(0,255,0),thickness=2)
+  return image_with_crosshair
+
+def react_to_target(image, landmark):  
+  
+  """ Processes landmark position and sends Twist msg to be used by dynamixels
+
+  Parameters
+  ----------
+  image: cv2 compatybile image format
+  landmark: Mediapipe landmark
+  
+  Returns
+  ----------
+  image: cv2 image with pink lines showing landmark position 
+  """
+  (h,w,c) = image.shape
+  (x,y,z,vis) = (landmark.x, landmark.y, landmark.z, landmark.visibility)
+  if (vis < 0.5):
+    return image
+  
+  # Sending msg to move dynamixels
+  command = Twist()
+  command.angular.z =-(x*2 - 1)           #Pan
+  command.angular.x = y*2 - 1             #Tilt
+  command_publisher.publish(command)
+  
+  #Draws a line form the center of the screen to the landmark position
+  x = x*w
+  y = y*h
+  debug_image = cv2.line(image,(int(w/2),int(h/2)),(int(x),int(y)),(255,0,255),thickness=3)
+  # debug_image = cv2.line(image,(int(w/2),int(h/2)),(int(x),int(h/2)),(255,0,255),thickness=3)
+  return debug_image
+
+# The main function of the code reads frames from the webcam, processes them using the MediaPipe pose model
+# and publishes Twist commands to control the Nerf turret. It limits the maximum FPS and draws a crosshair on the center of the screen.
+# Exits when the user presses the 'q' key or 'esc'
+
+with mp_pose.Pose(
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5,
+    model_complexity=0) as pose:
+  while cap.isOpened():
+    if (time.time()- startTime) > 1/fps_limit:
+      
+      # getting video frame
+      success, image = cap.read()
+      startTime = time.time()
+      image = cv2.resize(image,(640,480))
+      if not success:
+        print("Ignoring empty camera frame.")
+        # If loading a video, use 'break' instead of 'continue'.
+        continue
+      
+      # boosting processing performance
+      image.flags.writeable = False
+      image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+      # processing the frame
+      results = pose.process(image)
+
+      # Draw the pose annotation on the image.
+      image.flags.writeable = True
+      image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+      mp_drawing.draw_landmarks(
+          image,
+          results.pose_landmarks,
+          mp_pose.POSE_CONNECTIONS,
+          landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+      if results.pose_landmarks:
+        # If a human has been detected: react
+        image = react_to_target(image, results.pose_landmarks.landmark[0])
+        
+        
+      # Adding a crosshair to the image
+      image = draw_crosshair(image)
+      
+      # Showing the view from camera with crosshair and debug lines
+      cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
+      
+    key = cv2.waitKey(5)
+    if key == ord('q') or key == 27:
+      break
+    
+cap.release()
+cv2.destroyAllWindows()
+
--- src/turret_node_camera.cpp
+++ src/turret_node_camera.cpp
@@ -1,4 +1,8 @@
 
+// Sorry for making this thing one long file. Future me will fix it.
+// At least it's not like I've released the files so you can recreate the turret
+
+
 //As a base for this code I'm using one of the examples provided by ROBOTIS CO., LTD. 
 //for the use of dynamixels here. So this part stays.
 
@@ -20,7 +24,6 @@
 
 #include "std_msgs/String.h"
 #include "dynamixel_sdk_examples/GetPosition.h"
-#include "dynamixel_sdk_examples/SetPosition.h"
 #include "dynamixel_sdk/dynamixel_sdk.h"
 
 #include "sensor_msgs/Joy.h"
@@ -52,9 +55,9 @@ using namespace dynamixel;
 #define PROTOCOL_VERSION      2.0             // Default Protocol version of DYNAMIXEL X series.
 
 // Default setting
-#define DXL1_ID               1               // DXL1 ID
-#define DXL2_ID               2               // DXL2 ID
-#define DXL3_ID               3               // DXL3 ID
+#define DXL1_ID               1               // DXL1 ID - tilt left
+#define DXL2_ID               2               // DXL2 ID - pan
+#define DXL3_ID               3               // DXL3 ID - tilt right
 
 #define BAUDRATE              1000000         // Default Baudrate of DYNAMIXEL X series
 #define DEVICE_NAME           "/dev/ttyUSB0"  // [Linux] To find assigned port, use "$ ls /dev/ttyUSB*" command
@@ -66,7 +69,6 @@ using namespace dynamixel;
 
 //Flags
 bool a_button_pressed = false;
-
 int current_mode = 0;         // 0 - manual driving and aiming || 1 - manual driving, self aiming
 
 PortHandler * portHandler;
@@ -82,6 +84,10 @@ ros::Publisher ledControl_pub;
 std_msgs::Bool bool_msg;
 
 void toggle_led(int ledID){
+  /**
+   * Sends a message ment to toggle chosen LED
+  */
+
   std_msgs::Int8 msg;
   msg.data = ledID;
   ledControl_pub.publish(msg);
@@ -89,62 +95,39 @@ void toggle_led(int ledID){
 
 //Map value from one range into another
 float mapValue(float input, float minValInput, float maxValInput, float minValOut, float maxValOut){
+  /**
+   * Maps input value from input range to output range
+   * @param input - value to map
+   * @param minValInput - input range min Value
+   * @param maxValInput - input range max Value
+   * @param minValOut - input range min Value
+   * @param maxValOut - input range min Value
+   * @return mapped value
+  */
   return (input-minValInput)/(maxValInput-minValInput)*(maxValOut-minValOut)+minValOut;
 }
 
-bool getPresentPositionCallback(
-  dynamixel_sdk_examples::GetPosition::Request & req,
-  dynamixel_sdk_examples::GetPosition::Response & res)
-{
-  uint8_t dxl_error = 0;
-  int dxl_comm_result = COMM_TX_FAIL;
-  int32_t position = 0;
-
-  dxl_comm_result = packetHandler->read4ByteTxRx(
-    portHandler, (uint8_t)req.id, ADDR_PRESENT_POSITION, (uint32_t *)&position, &dxl_error);
-  if (dxl_comm_result == COMM_SUCCESS) {
-    //ROS_INFO("getPosition : [ID:%d] -> [POSITION:%d]", req.id, position);
-    res.position = position;
-    return true;
-  } else {
-    //ROS_INFO("Failed to get position! Result: %d", dxl_comm_result);
-    return false;
-  }
-}
-
-void setPositionCallback(const dynamixel_sdk_examples::SetPosition::ConstPtr & msg)
-{
-  uint8_t dxl_error = 0;
-  int dxl_comm_result = COMM_TX_FAIL;
-  uint32_t position = (unsigned int)msg->position;
-
-  dxl_comm_result = packetHandler->write4ByteTxRx(
-    portHandler, (uint8_t)msg->id, ADDR_GOAL_POSITION, position, &dxl_error);
-  if (dxl_comm_result == COMM_SUCCESS) {
-    //ROS_INFO("setPosition : [ID:%d] [POSITION:%d]", msg->id, msg->position);
-  } else {
-    ROS_ERROR("Failed to set position! Result: %d", dxl_comm_result);
-  }
-}
-
-
-//This one tells dynamixels how to move while taking into consideration the position they are currently in
 void updateDynamixel(int id, int speed, int minPos, int maxPos){
+  /**
+   *  Used to tell dynamixels how to move. Uses min/max pos to stop force the dynamixels to stop when moving out of bounds
+   * @param id - dynamixel ID
+   * @param speed - speed to move at (positive or negative depending on the direction)
+   * @param minPos - lower positional bound
+   * @param maxPos - upper positional bound
+  */
 
   uint8_t dxl_error = 0;
   int dxl_comm_result = COMM_TX_FAIL;
 
-  uint32_t position = 0;                                   //Where am i?
+  // Check current position
+  uint32_t position = 0;
   dxl_comm_result = packetHandler->read4ByteTxRx(
   portHandler, id, ADDR_PRESENT_POSITION, (uint32_t *)&position, &dxl_error);
+  // Compare with bounds, speed = 0 if OOB. Continue otherwise
+  if(position >= maxPos)          {speed > 0 ? speed = 0 : speed = speed;}
+  else if(position <= minPos )    {speed < 0 ? speed = 0 : speed = speed;}
 
-  if(position >= maxPos){                                 //Am i too far either way?   STOP : move as intended
-    speed > 0 ? speed = 0 : speed = speed;
-  }
-  else if(position <= minPos ){
-    speed < 0 ? speed = 0 : speed = speed;
-  }
-
+  // Update dynamixel speed
   dxl_comm_result = packetHandler->write4ByteTxRx(
     portHandler, (uint8_t)id, ADDR_GOAL_VELOCITY, speed, &dxl_error);
     if (dxl_comm_result == COMM_SUCCESS) {
@@ -155,14 +138,18 @@ void updateDynamixel(int id, int speed, int minPos, int maxPos){
 
 }
 
-//This one responds to every message sent to the /joy topic
-//Here it is used to control the turret
 void joyCallback(const sensor_msgs::Joy::ConstPtr & msg){
 
-  if(!msg->buttons[5]){return; }                                //turret enable button
+  /**
+   * Reacts to joy message.
+   * Moves / changes state of the turret if requested
+  */
 
-  if(!a_button_pressed && msg->buttons[0]){                     //select mode
-    //change used mode
+  // turret control enable button
+  if(!msg->buttons[5]){return; }                                
+
+  // turret operating mode selection
+  if(!a_button_pressed && msg->buttons[0]){                     
     current_mode++;
     current_mode = current_mode%2;
     toggle_led(GREENLED);
@@ -171,49 +158,51 @@ void joyCallback(const sensor_msgs::Joy::ConstPtr & msg){
 
   //Shooting
   lastShootCommand = ros::Time::now();
-  bool_msg.data = (msg->axes[2] < -0.8);       //Left trigger - spin the motors to build inertia if pressed
+  bool_msg.data = (msg->axes[2] < -0.8);                      //Left trigger - spin the motors to build inertia if pressed
   spin_pub.publish(bool_msg);
 
-  if( bool_msg.data && (msg->axes[5]<-0.8)){                        //if both Left and RIGHT trigger are pressed - use the other motor to fire the balls
+  if( bool_msg.data && (msg->axes[5]<-0.8)){                  //if both Left and right triggers are pressed - use the other motor to fire the balls
     bool_msg.data = true;
     trigger_pub.publish(bool_msg);
   }
-  else{                                                          //else make sure we are not jamming the balls between non spinning motors
-    bool_msg.data = false;
+  else{                                                       //else make sure we are not jamming the balls between non spinning motors
+    bool_msg.data = false; 
     trigger_pub.publish(bool_msg);
   }
 
   //Pan Tilt
+  if(current_mode == 1){return;}                               //If in autonomous control mode, return
 
-  if(current_mode == 1){return;}                                //If in camera controll mode, don't move
+  lastMoveCommand = ros::Time::now();                             //Set the last move command time to current time -> used in the watchdog
 
-  float msg_speed_x = msg->axes[3];                                 //Pan
-  float msg_speed_y = msg->axes[4];                                 //Tilt
+  float msg_speed_x = msg->axes[3];
+  float msg_speed_y = msg->axes[4];
 
   //Pn
-  int speed_id2 = mapValue(msg_speed_x,-1,1,40,-40);     
+  int speed_id2 = mapValue(msg_speed_x,-1,1,40,-40);              //Hardcoded motor speed as the last two parameters :) 
   //Tilt
   int speed_id1 = mapValue(msg_speed_y,-1,1,30,-30);              
   int speed_id3 = mapValue(msg_speed_y,-1,1,-30,30);      
 
-  lastMoveCommand = ros::Time::now();                              //Set the last move command time to current time -> used in the watchdog
-
   //Pan
   updateDynamixel(2,speed_id2,200,3900);                          //Hardcoded min and max position for pan as the last two parameters :)
-
   //Tilt
-  updateDynamixel(1,speed_id1,1900,2350);                         //Hardcoded min and max position for tilt as the last two parametes :) 
+  updateDynamixel(1,speed_id1,1900,2350);
   updateDynamixel(3,speed_id3,1750,2200);
 }
 
 void cameraCallback(const geometry_msgs::Twist::ConstPtr & msg){
+  /**
+   * Reacts to target detector message.
+   * Makes the dynamixels move at the speed requested by it.
+  */
 
-  if(current_mode == 0){return;}
+  if(current_mode == 0){return;}                                //If not in autonoums control mode, return
 
-  float msg_speed_x = msg->angular.z;                               //Pan
-  float msg_speed_y = msg->angular.x;                               //Tilt
+  float msg_speed_x = msg->angular.z;
+  float msg_speed_y = msg->angular.x;
 
-  int speed_id1 = mapValue(msg_speed_y,-1,1,30,-30);
+  int speed_id1 = mapValue(msg_speed_y,-1,1,30,-30);              //Hardcoded motor speed as the last two parameters :) 
   int speed_id2 = mapValue(msg_speed_x,-1,1,40,-40);
   int speed_id3 = mapValue(msg_speed_y,-1,1,-30,30);
 
@@ -221,14 +210,15 @@ void cameraCallback(const geometry_msgs::Twist::ConstPtr & msg){
 
   //Pan
   updateDynamixel(2,speed_id2,200,3900);                          //Hardcoded min and max position for pan as the last two parameters :)
-
   //Tilt
-  updateDynamixel(1,speed_id1,1900,2350);                         //Hardcoded min and max position for tilt as the last two parametes :) 
+  updateDynamixel(1,speed_id1,1900,2350);
   updateDynamixel(3,speed_id3,1750,2200);
 }
 
-//Changes the PID Values while in position control mode
 void changePositionPIDValues(int id, uint16_t kp, uint16_t ki, uint16_t kd){
+  /**
+   * Changes the dynamixel PID Values in position control mode
+  */
 
   uint8_t dxl_error = 0;
   int dxl_comm_result = COMM_TX_FAIL;
@@ -250,11 +240,12 @@ void changePositionPIDValues(int id, uint16_t kp, uint16_t ki, uint16_t kd){
   if (dxl_comm_result != COMM_SUCCESS) {
     ROS_ERROR("Failed to change pos kd %d", id);
   }
-
 }
 
-//Changes the PI Values while in wheel control mode
 void changeVelocityPIValues(int id, uint16_t kp, uint16_t ki){
+  /**
+   * Changes the dynamixel PI Values in speed control mode
+  */
 
   uint8_t dxl_error = 0;
   int dxl_comm_result = COMM_TX_FAIL;
@@ -270,11 +261,13 @@ void changeVelocityPIValues(int id, uint16_t kp, uint16_t ki){
   if (dxl_comm_result != COMM_SUCCESS) {
     ROS_ERROR("Failed to change vel ki %d", id);
   }
-
 }
 
-//Shows the PID Values for position control mode
 void readPositionPIDValues(int id){
+  /**
+   * Reads and shows the dynamixel position control PID Values
+  */
+
    uint8_t dxl_error = 0;
   int dxl_comm_result = COMM_TX_FAIL;
 
@@ -294,8 +287,10 @@ void readPositionPIDValues(int id){
   ROS_INFO("POS PID: [ID:%d] [P:%d] [I:%d] [D:%d]", id, kp, ki, kd);
 }
 
-//Shows the PI Values for wheel control mode
 void readVelocityPIValues(int id){
+  /**
+   * Reads and shows the dynamixel speed control PI Values
+  */
   uint8_t dxl_error = 0;
   int dxl_comm_result = COMM_TX_FAIL;
 
@@ -312,106 +307,13 @@ void readVelocityPIValues(int id){
 
 }
 
-//Makes the dynamixels go to the chosen position 
-//This one looks like shit, but works all right.
-//It's filled to the brim with hard coded values and repetitive tasks. 
-bool homeDynamixelCallback(  dynamixel_sdk_examples::GetPosition::Request & req,  dynamixel_sdk_examples::GetPosition::Response & res){
-  //Not yet changed for 3 dynamixels
-  return false;
-
-  uint8_t dxl_error = 0;
-  int dxl_comm_result = COMM_TX_FAIL;
-  int32_t position = 0;
-
-  if(req.id == 1){position = 1700;}               //hard coded home value for dynamixel ID 1
-  else if (req.id == 2){position = 2048;}         //same thing for dynamixel ID 2
-
-  res.position = position;
-
-  //Turn off torque
-  dxl_comm_result = packetHandler->write1ByteTxRx(
-  portHandler, req.id, ADDR_TORQUE_ENABLE, 0, &dxl_error);
-  if (dxl_comm_result != COMM_SUCCESS) {
-    ROS_ERROR("Failed to disable torque for Dynamixel ID %d", req.id);
-    return -1;
-  }
-
-  //Change the mode to position control
-  dxl_comm_result = packetHandler->write1ByteTxRx(
-    portHandler, req.id , ADDR_OPERATING_MODE , 3, &dxl_error);
-  if (dxl_comm_result != COMM_SUCCESS) {
-    ROS_ERROR("Failed to change to position control ID %d", req.id);
-    return false;
-  }
-
-  //Turn on torque
-  dxl_comm_result = packetHandler->write1ByteTxRx(
-    portHandler, req.id, ADDR_TORQUE_ENABLE, 1, &dxl_error);
-  if (dxl_comm_result != COMM_SUCCESS) {
-    ROS_ERROR("Failed to enable torque for Dynamixel ID %d", req.id);
-    return -1;
-  }
-
-  changePositionPIDValues(req.id, 150, 0, 3600);                  //again hard coded values for a fast but not too snappy homing.
-  //ROS_INFO("changed pos PID for: [ID:%d] ", req.id);
-  readPositionPIDValues(req.id);
-
-  //Set goal position
-  dxl_comm_result = packetHandler->write4ByteTxRx(
-    portHandler, req.id, ADDR_GOAL_POSITION, position, &dxl_error);
-  if (dxl_comm_result == COMM_SUCCESS) {
-    //ROS_INFO("setPosition : [ID:%d] [POSITION:%d]", req.id, position);
-  } else {
-    ROS_ERROR("Failed to set position! Result: %d", dxl_comm_result);
-    return false;
-  }
-
-  //wait time necessary for the dynamixel to complete the task
-  ros::Duration(2.0).sleep();
-
-  //Go back to velocity control
-
-  //Turn off torque
-  dxl_comm_result = packetHandler->write1ByteTxRx(
-    portHandler, req.id, ADDR_TORQUE_ENABLE, 0, &dxl_error);
-  if (dxl_comm_result != COMM_SUCCESS) {
-    ROS_ERROR("Failed to disable torque for Dynamixel ID %d", req.id);
-    return -1;
-  }
-
-  //Turn on velocity control
-  dxl_comm_result = packetHandler->write1ByteTxRx(
-    portHandler, req.id, ADDR_OPERATING_MODE , 1, &dxl_error);
-  if (dxl_comm_result != COMM_SUCCESS) {
-    ROS_ERROR("Failed to change to position control ID %d", req.id);
-    return false;
-  }
-
-  //Turn on torque
-  dxl_comm_result = packetHandler->write1ByteTxRx(
-    portHandler, req.id, ADDR_TORQUE_ENABLE, 1, &dxl_error);
-  if (dxl_comm_result != COMM_SUCCESS) {
-    ROS_ERROR("Failed to enable torque for Dynamixel ID %d", req.id);
-    return -1;
-  }
-
-  changeVelocityPIValues(req.id, 400, 3000);
-  //ROS_INFO("changed vel PI for: [ID:%d]", req.id);
-  readVelocityPIValues(req.id);
-
-  return true;
-}
-
 int main(int argc, char ** argv)
 {
 
   //ROS setup
   ros::init(argc, argv, "nerf_turret_node");
   ros::NodeHandle nh;
-  ros::ServiceServer get_position_srv =     nh.advertiseService("/get_position", getPresentPositionCallback);
-  ros::ServiceServer home_dynamixel_srv =   nh.advertiseService("/home_dynamixel", homeDynamixelCallback);
   
-  ros::Subscriber set_position_sub =        nh.subscribe("/set_position", 10, setPositionCallback);
   ros::Subscriber set_speed_sub =           nh.subscribe("/joy", 10, joyCallback);
   ros::Subscriber camera_command_sub =      nh.subscribe("/nerf_turret/command",1,cameraCallback);
 
@@ -441,6 +343,7 @@ int main(int argc, char ** argv)
     return -1;
   }
 
+  //This should not be repeated 3 times
   dxl_comm_result = packetHandler->write1ByteTxRx(
     portHandler, DXL1_ID, ADDR_TORQUE_ENABLE, 1, &dxl_error);
   if (dxl_comm_result != COMM_SUCCESS) {
@@ -469,9 +372,9 @@ int main(int argc, char ** argv)
     readPositionPIDValues(i);
   }
 
-  //Really ugly way of ensuring that the Arduino I've nearly killed is up and running
-  //before i send any message to the topics it's subscribing to <-- makes it go craaaaazy
-  //60 second delay
+  // Really ugly way of ensuring that the Arduino I've nearly killed is up and running
+  // before i send any message to the topics it's subscribing to <-- makes it go craaaaazy
+  // 60 second delay
   lastMoveCommand = ros::Time::now();
   toggle_led(REDLED);
   while(ros::Time::now() - lastMoveCommand < ros::Duration(60)){};
@@ -489,25 +392,24 @@ int main(int argc, char ** argv)
 
   while(ros::ok())
  {
-    //If no command was sent in the last 0.3s
+  //If no joy command was sent in the last 0.3s
   if((ros::Time::now() - lastMoveCommand) > ros::Duration(0.3)){
     lastMoveCommand = ros::Time::now();
 
-    //stop the dynamixels
-    updateDynamixel(2,0,100,4000);
-    updateDynamixel(1,0,1900,2350);                          //Hardcoded min and max position for tilt as the last two parametes :) 
-    updateDynamixel(3,0,1750,2200);
-  }
+    //stop the pan//tilt dynamixels
+    updateDynamixel(DXL2_ID,0,100,4000);
+    updateDynamixel(DXL1_ID,0,1900,2350);                          //Hardcoded min and max position for tilt as the last two parametes :) 
+    updateDynamixel(DXL3_ID,0,1750,2200);
+    }
   if((ros::Time::now() - lastShootCommand) > ros::Duration(0.2)){
     //stop the NERF blaster motors
     bool_msg.data = false;
     trigger_pub.publish(bool_msg);
     spin_pub.publish(bool_msg);
     lastShootCommand = ros::Time::now();
-    ROS_WARN("NOW");
-   }
+  }
 
-   ros::spinOnce();               
+  ros::spinOnce();               
  }
   portHandler->closePort();
   return 0;
